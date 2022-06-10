d103448eb9ddbb193105170bab9b11af192d53e1
Bennjoe <95551073+B3nnjoe@users.noreply.github.com>
2022-06-11 01:48:09 +0200

    init

---
+++ index.ts
@@ -0,0 +1,87 @@
+exports = function (language: String) {
+  //languages
+  const languages: Array<any> = [
+    {
+      lang: 'c',
+      solution: 'printf("Hello World\n");',
+    },
+    {
+      lang: 'c++',
+      solution: 'cout << "Hello, World!";',
+    },
+    {
+      lang: 'c#',
+      solution: 'System.Console.WriteLine("Hello World!");',
+    },
+    {
+      lang: 'java',
+      solution: 'System.out.println( "Hello World!" );',
+    },
+    {
+      lang: 'php',
+      solution: '<?php echo "Hello World!"; ?>',
+    },
+    {
+      lang: 'python',
+      solution: 'print("Hello World!")',
+    },
+    {
+      lang: 'ruby',
+      solution: 'puts "Hello World!"',
+    },
+    {
+      lang: 'swift',
+      solution: 'print("Hello World!")',
+    },
+    {
+      lang: 'kotlin',
+      solution: 'println("Hello World!")',
+    },
+    {
+      lang: 'typescript',
+      solution: 'console.log("Hello World!")',
+    },
+    {
+      lang: 'javascript',
+      solution: 'console.log("Hello World!")',
+    },
+    {
+      lang: 'go',
+      solution: 'fmt.Println("Hello World!")',
+    },
+    {
+      lang: 'rust',
+      solution: 'println!("Hello World!")',
+    },
+    {
+      lang: 'haskell',
+      solution: 'putStrLn "Hello World!"',
+    },
+    {
+      lang: 'elixir',
+      solution: 'IO.puts "Hello World!"',
+    },
+    {
+      lang: 'elm',
+      solution: 'main = putStrLn "Hello World!"',
+    },
+    {
+      lang: 'clojure',
+      solution: 'println "Hello World!"',
+    },
+    {
+      lang: 'scala',
+      solution: 'println("Hello World!")',
+    },
+  ];
+
+  // The result
+  let result = 'error: Language not supported';
+  // Loops trough all langs and sets the result to the langauges solution
+  languages.forEach((lang) => {
+    if (lang.lang === language.toLowerCase()) {
+      result = lang.solution;
+    }
+  });
+  return result;
+};
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Hello World In Diffrent Langs
+
+With this advanced package you can get how to print hello world in diffrent languages.
+
+### Installation
+
+Simply run the command in your terminal:
+```
+npm install @bennjoe/hello-world
+```
+
+### Usage
+
+```ts
+const helloWorld = require('@bennjoe/hello-world')
+
+console.log(helloWorld('c++'))
+```
+
